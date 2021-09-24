fa77a4a04c992812a01ccfb227e7e879fcc68798
myleshansen <myleshunter@hotmail.com>
2021-09-24 13:31:31 -0600

    display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,11 +3,27 @@
 
 std::vector<std::vector<std::string>> CreateBoard()
 {
-    std::vector<std::vector<std::string>> ret(3);
+    std::vector<std::vector<std::string>> ret{
+        {"-", "-", "-"},
+        {"-", "-", "-"},
+        {"-", "-", "-"}};
     return ret;
+}
+
+void DisplayBoard(std::vector<std::vector<std::string>> matrix)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main()
 {
     std::vector<std::vector<std::string>> matrix = CreateBoard();
+    DisplayBoard(matrix);
 }
