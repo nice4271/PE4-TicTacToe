ae2ab9f06ab01c5297ea365803389b54fc02e1c6
nice4271 <nice4271@colorado.edu>
2021-09-24 13:28:49 -0600

    DisplayBoard() Func

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <vector>
 
+
 std::vector<std::vector<std::string>> CreateBoard()
 {
-    std::vector<std::vector<std::string>> ret(3);
+    std::vector<std::vector<std::string>> ret{
+        {"-", "-", "-"},
+        {"-", "-", "-"},
+        {"-", "-", "-"}
+    };
     return ret;
+}
+
+
+void displayBoard(std::vector<std::vector<std::string>> V){
+    for(int i = 0; i < V.size(); i++){
+        for(int j = 0; j < V[i].size(); j++){
+            std::cout << V[i][j];
+        }
+        std::cout << std::endl;
+    }
+    return;
 }
 
 int main()
 {
     std::vector<std::vector<std::string>> matrix = CreateBoard();
+
+    displayBoard(matrix);
+
+    return 0;
 }
